b99b69f8a3c1631804ca09f3ea72c0aa9dfc6ba8
Will <william.guiracoche@gmail.com>
2018-10-24 23:48:14 -0400

    feat: Create database_setup.py to make Genre and Song classes

---
+++ database_setup.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Genre(Base):
+    __tablename__ = 'genre'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Song(Base):
+    __tablename__ = 'song'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    artist = Column(String(80))
+    album = Column(String(80))
+    video_url = Column(String(250))
+    genre_id = Column(Integer, ForeignKey('genre.id'))
+    genre = relationship(Genre)
+
+engine = create_engine('sqlite:///music.db')
+
+
+Base.metadata.create_all(engine)
