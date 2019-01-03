19c0c962e27724c3214b5af9c99bbd0c2240fe4f
Will <william.guiracoche@gmail.com>
2019-01-03 11:52:06 -0500

    feat: Add user info to db_fill.py

--- db_fill.py
+++ db_fill.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Base, Genre, Song
+from database_setup import Base, Genre, Song, User
 
 engine = create_engine('sqlite:///music.db')
 # Bind the engine to the metadata of the Base class so that the
@@ -18,80 +18,82 @@ DBSession = sessionmaker(bind=engine)
 # session.rollback()
 session = DBSession()
 
+user1 = User(id=1, name="will", email="will.email.com")
+
 #Playlist for Rock
-genre1 = Genre(name="Rock")
+genre1 = Genre(name="Rock", user=user1)
 session.add(genre1)
 session.commit()
 
-song1 = Song(name="Mr. Brightside", artist="The Killers", album="Hot Fuss", genre=genre1)
+song1 = Song(name="Mr. Brightside", artist="The Killers", album="Hot Fuss", genre=genre1, user=user1)
 session.add(song1)
 session.commit()
 
-song2 = Song(name="Seven Nation Army", artist="The White Stripes", album="Elephant", genre=genre1)
+song2 = Song(name="Seven Nation Army", artist="The White Stripes", album="Elephant", genre=genre1, user=user1)
 session.add(song2)
 session.commit()
 
-song3 = Song(name="Uprising", artist="Muse", album="The Resistance", genre=genre1)
+song3 = Song(name="Uprising", artist="Muse", album="The Resistance", genre=genre1, user=user1)
 session.add(song3)
 session.commit()
 
-song4 = Song(name="Do I Wanna Know?", artist="Arctic Monkeys", album="AM", genre=genre1)
+song4 = Song(name="Do I Wanna Know?", artist="Arctic Monkeys", album="AM", genre=genre1, user=user1)
 session.add(song4)
 session.commit()
 
-song5 = Song(name="Welcome to the Black Parade", artist="My Chemical Romance", album="The Black Parade", genre=genre1)
+song5 = Song(name="Welcome to the Black Parade", artist="My Chemical Romance", album="The Black Parade", genre=genre1, user=user1)
 session.add(song5)
 session.commit()
 
 
 #Playlist for Hip Hop
-genre2 = Genre(name="Hip Hop")
+genre2 = Genre(name="Hip Hop", user=user1)
 session.add(genre2)
 session.commit()
 
-song6 = Song(name="Poetic Justice", artist="Kendrick Lamar", album="good kid, m.A.A.d. City", genre=genre2)
+song6 = Song(name="Poetic Justice", artist="Kendrick Lamar", album="good kid, m.A.A.d. City", genre=genre2, user=user1)
 session.add(song6)
 session.commit()
 
 
-song7 = Song(name="No Role Modelz", artist="J. Cole", album="2014 Forest Hills Drive", genre=genre2)
+song7 = Song(name="No Role Modelz", artist="J. Cole", album="2014 Forest Hills Drive", genre=genre2, user=user1)
 session.add(song7)
 session.commit()
 
-song8 = Song(name="No Problem", artist="Chance the Rapper", album="Coloring Book", genre=genre2)
+song8 = Song(name="No Problem", artist="Chance the Rapper", album="Coloring Book", genre=genre2, user=user1)
 session.add(song8)
 session.commit()
 
-song9 = Song(name="Empire State of Mind", artist="Jay-Z", album="The Blueprint 3", genre=genre2)
+song9 = Song(name="Empire State of Mind", artist="Jay-Z", album="The Blueprint 3", genre=genre2, user=user1)
 session.add(song9)
 session.commit()
 
-song10 = Song(name="Sanctified", artist="Rick Ross", album="Mastermind", genre=genre2)
+song10 = Song(name="Sanctified", artist="Rick Ross", album="Mastermind", genre=genre2, user=user1)
 session.add(song10)
 session.commit()
 
 #Playlist for Electronic
-genre3 = Genre(name="Electronic")
+genre3 = Genre(name="Electronic", user=user1)
 session.add(genre3)
 session.commit()
 
-song11 = Song(name="Adagio for Strings", artist="Tiesto", album="Just Be", genre=genre3)
+song11 = Song(name="Adagio for Strings", artist="Tiesto", album="Just Be", genre=genre3, user=user1)
 session.add(song11)
 session.commit()
 
-song12 = Song(name="Clarity", artist="Zedd", album="Clarity", genre=genre3)
+song12 = Song(name="Clarity", artist="Zedd", album="Clarity", genre=genre3, user=user1)
 session.add(song12)
 session.commit()
 
-song13 = Song(name="Titanium", artist="David Guetta", album="Nothing but the Beat", genre=genre3)
+song13 = Song(name="Titanium", artist="David Guetta", album="Nothing but the Beat", genre=genre3, user=user1)
 session.add(song13)
 session.commit()
 
-song14 = Song(name="Don't You Worry Child", artist="Swedish House Mafia", album="Until Now", genre=genre3)
+song14 = Song(name="Don't You Worry Child", artist="Swedish House Mafia", album="Until Now", genre=genre3, user=user1)
 session.add(song14)
 session.commit()
 
-song15 = Song(name="Wake Me Up", artist="Avicii", album="True", genre=genre3)
+song15 = Song(name="Wake Me Up", artist="Avicii", album="True", genre=genre3, user=user1)
 session.add(song15)
 session.commit()
 
